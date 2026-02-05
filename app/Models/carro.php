47d3f06472f024c869b13db9c63e6ce1f3ce6965
João Pedro Gouvea <joaopedrosouza1324@gmail.com>
2026-02-05 18:16:27 -0300

    first commit

---
+++ app/Models/carro.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class carro extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['marca','modelo','ano','placa','preco_diaria',];
+}
---
+++ database/migrations/2026_02_05_002424_create_carros_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('carros', function (Blueprint $table) {
+            $table->id();
+            $table->string('marca');
+            $table->string('modelo');
+            $table->year('ano');
+            $table->string('placa')->unique();
+            $table->decimal('preco_diaria', 10, 2);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('carros');
+    }
+};
